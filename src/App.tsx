122e3c3935fd438be9aaee4edf10fa57422bcb57
Aleksey Kriuchkov <aleksejkriuchkov@gmail.com>
2023-06-07 19:01:41 +0300

    add routes for auth pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,8 @@
 import "./App.css"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import { Counter } from "./features/counter/Counter"
-import {
-  CheckEmail,
-  ForgotPassword,
-  NewPassword,
-  Profile,
-  ResetPassword,
-  SignIn,
-  SignUp,
-} from "@/modules/auth/pages"
-import { ErrorPage } from "@/modules/auth/pages/error404"
+import { RouterProvider } from "react-router-dom"
+import { route } from "@/routes"
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Counter />,
-  },
-  {
-    path: "/check-email",
-    element: <CheckEmail />,
-  },
-  {
-    path: "/forgot-password",
-    element: <ForgotPassword />,
-  },
-  {
-    path: "/new-password",
-    element: <NewPassword />,
-  },
-  {
-    path: "/profile",
-    element: <Profile />,
-  },
-  {
-    path: "/reset-password",
-    element: <ResetPassword />,
-  },
-  {
-    path: "/sign-in",
-    element: <SignIn />,
-  },
-  {
-    path: "/sign-up",
-    element: <SignUp />,
-  },
-  {
-    path: "*",
-    element: <ErrorPage />,
-  },
-])
-
-function App() {
+const router = route()
+export const App = () => {
   return <RouterProvider router={router} />
 }
-
-export default App
