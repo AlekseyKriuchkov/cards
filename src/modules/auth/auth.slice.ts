c4ab1f67a8f364c17be0992a04b5237299d29b7c
Aleksey Kriuchkov <aleksejkriuchkov@gmail.com>
2023-07-23 17:07:58 +0300

    add login logic in auth slice

--- src/modules/auth/auth.slice.ts
+++ src/modules/auth/auth.slice.ts
@@ -1,18 +1,26 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import { authApi } from "@/modules/auth/api/auth.api"
-import { LoginType, RegisterType } from "@/modules/auth/api/types"
+import {
+  LoginResponse,
+  LoginType,
+  RegisterType,
+} from "@/modules/auth/api/types"
+import { createAppAsyncThunk } from "@/utils/create-app-async-thunk"
 
-const register = createAsyncThunk("auth/register", (arg: RegisterType) => {
+const register = createAppAsyncThunk("auth/register", (arg: RegisterType) => {
   authApi.register(arg).then((res) => {
     console.log(res)
   })
 })
-const login = createAsyncThunk("auth/login", (arg: LoginType) => {
-  authApi.login(arg).then((res) => {
-    console.log(res)
-  })
-})
-const authMe = createAsyncThunk("authMe/login", () => {
+const login = createAppAsyncThunk<{ profile: LoginResponse }, LoginType>(
+  "auth/login",
+  async (arg) => {
+    const response = await authApi.login(arg)
+    console.log(response.data)
+    return { profile: response.data }
+  },
+)
+const authMe = createAppAsyncThunk("authMe/login", () => {
   authApi.authMe().then((res) => {
     console.log(res)
   })
@@ -20,10 +28,18 @@ const authMe = createAsyncThunk("authMe/login", () => {
 
 const slice = createSlice({
   name: "auth",
-  initialState: {},
-  reducers: {},
+  initialState: {
+    profile: null as LoginResponse | null,
+  },
+  reducers: {
+    setProfile: (state, action: PayloadAction<{ profile: LoginResponse }>) => {
+      state.profile = action.payload.profile
+    },
+  },
 })
 
 export const authReducer = slice.reducer
+
+export const authActions = slice.actions
 
 export const authThunk = { register, login, authMe }
