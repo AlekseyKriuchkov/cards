0b44031b765bb646379b10e9d8c473cf9ae3d7fa
Aleksey Kriuchkov <aleksejkriuchkov@gmail.com>
2023-07-23 17:06:20 +0300

    add sign in page

--- src/modules/auth/pages/sign-in/index.tsx
+++ src/modules/auth/pages/sign-in/index.tsx
@@ -1,23 +1,41 @@
-import React, { useEffect } from "react"
-import { useAppDispatch, useAppSelector } from "@/app/hooks"
-import { appActions, isLoading } from "@/app/app.slice"
+import { useAppDispatch } from "@/app/hooks"
 import { authThunk } from "@/modules/auth/auth.slice"
 import { StyledCard } from "@/modules/auth/pages/sign-in/styles"
+import { Form, Input, Button, Checkbox } from "antd"
+import { LoginType } from "@/modules/auth/api/types"
 
 export const SignIn = () => {
+  const dispatch = useAppDispatch()
+
+  const onFinish = (values: LoginType) => {
+    dispatch(authThunk.login(values))
+  }
+
   return (
-    <>
-      <StyledCard title={"Sign In"} />
-    </>
+    <StyledCard title={"Sign In"}>
+      <Form name="basic" onFinish={onFinish} autoComplete="off">
+        <Form.Item name="email">
+          <Input placeholder={"Email"} />
+        </Form.Item>
+
+        <Form.Item name="password">
+          <Input.Password placeholder={"Password"} />
+        </Form.Item>
+
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8 }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" size={"large"} block={true}>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </StyledCard>
   )
 }
-
-// const dispatch = useAppDispatch()
-// const isLoading2 = useAppSelector(isLoading)
-//
-// useEffect(() => {
-//   setTimeout(() => {
-//     dispatch(appActions.setIsLoading({ isLoading: false }))
-//   }, 3000)
-//   dispatch(authThunk.authMe())
-// }, [])
