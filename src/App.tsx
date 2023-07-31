595b5f9192906cb02a5b5b65b8e697864a2edda8
Aleksey Kriuchkov <aleksejkriuchkov@gmail.com>
2023-07-31 13:04:18 +0300

    add me request in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import { route } from "@/routes"
 import { Header } from "@/modules/header"
 import { GlobalStyle } from "@/global-style"
 import { ContentWrapper } from "@/modules/auth/pages/styles"
+import { useAppDispatch } from "@/app/hooks"
+import { authThunk } from "@/modules/auth/auth.slice"
 
 const router = route()
 export const App = () => {
+  const dispatch = useAppDispatch()
+  dispatch(authThunk.authMe())
   return (
     <>
       <GlobalStyle />
