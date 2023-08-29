621243efe3d569fb005baccfa862d3388a978a0f
Aleksey Kriuchkov <aleksejkriuchkov@gmail.com>
2023-08-29 17:43:44 +0300

    refactor styles new pack modal

--- src/modules/cards/components/add-new-pack-modal/add-new-pack-modal.tsx
+++ src/modules/cards/components/add-new-pack-modal/add-new-pack-modal.tsx
@@ -1,7 +1,11 @@
 import React from "react"
-import { Button, Form, Input } from "antd"
+import { Button, Checkbox, Form, Input } from "antd"
 import { useAppDispatch } from "@/app/hooks"
 import { cardsThunk } from "@/modules/cards/cards.slice"
+import {
+  StyledAddNewPackModalButtonsWrapper,
+  StyledAddNewPackModalWrapper,
+} from "@/modules/cards/components/add-new-pack-modal/styles"
 
 type PropsType = {
   callback: () => void
@@ -10,23 +14,38 @@ type PropsType = {
 export const AddNewPackModal: React.FC<PropsType> = ({ callback }) => {
   const dispatch = useAppDispatch()
   const onFinish = async (values: { value: string }) => {
-    dispatch(cardsThunk.newPack({ cardsPack: { name: values.value } }))
+    dispatch(
+      cardsThunk.newPack({
+        cardsPack: { name: values.value, private: !!values.value },
+      }),
+    )
     callback()
   }
   const handleCancel = () => {
     callback()
   }
   return (
-    <Form onFinish={onFinish}>
-      <Form.Item name="value">
-        <Input placeholder={"Enter pack name"} />
-      </Form.Item>
-      <Form.Item>
-        <Button htmlType="submit">Submit</Button>
-      </Form.Item>
-      <Form.Item>
-        <Button onClick={handleCancel}>Cancel</Button>
-      </Form.Item>
-    </Form>
+    <StyledAddNewPackModalWrapper>
+      <Form onFinish={onFinish}>
+        <Form.Item name="value">
+          <Input placeholder={"Enter pack name"} />
+        </Form.Item>
+        <Form.Item name="private" valuePropName="checked">
+          <Checkbox>Private</Checkbox>
+        </Form.Item>
+        <StyledAddNewPackModalButtonsWrapper>
+          <Form.Item>
+            <Button type={"primary"} htmlType="submit">
+              Add pack
+            </Button>
+          </Form.Item>
+          <Form.Item>
+            <Button type={"default"} onClick={handleCancel}>
+              Cancel
+            </Button>
+          </Form.Item>
+        </StyledAddNewPackModalButtonsWrapper>
+      </Form>
+    </StyledAddNewPackModalWrapper>
   )
 }
---
+++ src/modules/cards/components/add-new-pack-modal/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const StyledAddNewPackModalWrapper = styled.div`
+  padding-top: 20px;
+`
+export const StyledAddNewPackModalButtonsWrapper = styled.div`
+  display: flex;
+  padding-left: 60%;
+  justify-content: space-between;
+`
