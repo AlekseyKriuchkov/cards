c75d0b1c84c65a7dc8147ec4fd816fdef96df8cd
Aleksey Kriuchkov <aleksejkriuchkov@gmail.com>
2024-08-07 19:44:52 +0200

    refactor learn grade

--- src/modules/cards/components/learn-card/learn-card.tsx
+++ src/modules/cards/components/learn-card/learn-card.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react"
-import { LearnAnswer } from "@/modules/cards/components/learn-answer/learn-answer"
 import { LearnSuccess } from "@/modules/cards/components/learn-success/learn-success"
 import {
   StyledLearnCardButton,
   StyledLearnQuestion,
   StyledLearnQuestionWrapper,
 } from "@/modules/cards/components/learn-page/styles"
-import { Skeleton, Typography } from "antd"
+import { Rate, Skeleton, Typography } from "antd"
 import { CardType } from "@/modules/cards/types"
-import { useAppSelector } from "@/app/hooks"
+import { useAppDispatch, useAppSelector } from "@/app/hooks"
+import { cardsThunk } from "@/modules/cards/cards.slice"
 
 const { Text } = Typography
 
@@ -17,18 +17,29 @@ type PropsType = {
 }
 
 export const LearnCard: React.FC<PropsType> = ({ sortedCards }) => {
+  const dispatch = useAppDispatch()
+
   const isLoading = useAppSelector((state) => state.cards.isLoading)
 
   const [isShowAnswer, setIsShowAnswer] = useState(false)
 
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
 
-  const handleShowAnswer = () => {
-    setIsShowAnswer(true)
+  const [questionGrade, setQuestionGrade] = useState(0)
+
+  const handleNextQuestion = () => {
+    dispatch(
+      cardsThunk.updateGrade({
+        grade: questionGrade || sortedCards[currentQuestionIndex].grade,
+        card_id: sortedCards[currentQuestionIndex]._id,
+      }),
+    )
+    setIsShowAnswer(false)
+    setCurrentQuestionIndex(currentQuestionIndex + 1)
   }
 
-  if (isLoading) {
-    return <Skeleton active title={false} />
+  const handleShowAnswer = () => {
+    setIsShowAnswer(true)
   }
 
   if (currentQuestionIndex >= sortedCards.length || currentQuestionIndex >= 10)
@@ -36,28 +47,47 @@ export const LearnCard: React.FC<PropsType> = ({ sortedCards }) => {
 
   return (
     <>
-      {!isShowAnswer ? (
-        <>
-          <StyledLearnQuestionWrapper>
-            <StyledLearnQuestion>
-              <div>
-                <Text strong>Question: </Text>
-                <Text>{sortedCards[currentQuestionIndex].question}</Text>
-              </div>
-              <StyledLearnCardButton onClick={handleShowAnswer}>
-                Show answer
-              </StyledLearnCardButton>
-            </StyledLearnQuestion>
-          </StyledLearnQuestionWrapper>
-        </>
-      ) : (
-        <LearnAnswer
-          sortedCards={sortedCards}
-          setIsShowAnswer={setIsShowAnswer}
-          currentQuestionIndex={currentQuestionIndex}
-          setCurrentQuestionIndex={setCurrentQuestionIndex}
-        />
-      )}
+      <StyledLearnQuestionWrapper>
+        <StyledLearnQuestion>
+          {isLoading ? (
+            <Skeleton active title={false} />
+          ) : (
+            <>
+              {!isShowAnswer ? (
+                <>
+                  <div>
+                    <Text strong>Question: </Text>
+                    <Text>{sortedCards[currentQuestionIndex].question}</Text>
+                  </div>
+                  <StyledLearnCardButton onClick={handleShowAnswer}>
+                    Show answer
+                  </StyledLearnCardButton>
+                </>
+              ) : (
+                <>
+                  <div>
+                    <Text strong>Question: </Text>
+                    <Text>{sortedCards[currentQuestionIndex].question}</Text>
+                  </div>
+                  <div>
+                    <Text strong>Answer: </Text>
+                    <Text>{sortedCards[currentQuestionIndex].answer}</Text>
+                  </div>
+                  <Rate
+                    defaultValue={sortedCards[currentQuestionIndex].grade}
+                    onChange={(value) => {
+                      setQuestionGrade(value)
+                    }}
+                  />
+                  <StyledLearnCardButton onClick={handleNextQuestion}>
+                    Next Question
+                  </StyledLearnCardButton>
+                </>
+              )}
+            </>
+          )}
+        </StyledLearnQuestion>
+      </StyledLearnQuestionWrapper>
     </>
   )
 }
