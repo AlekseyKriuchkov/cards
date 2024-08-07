38d299b2f65093ce0d35c7dc176e97477e91b263
Aleksey Kriuchkov <aleksejkriuchkov@gmail.com>
2024-08-07 20:09:21 +0200

    add learn success

--- src/modules/cards/components/learn-success/learn-success.tsx
+++ src/modules/cards/components/learn-success/learn-success.tsx
@@ -1,10 +1,26 @@
 import React from "react"
-import { HeartOutlined } from "@ant-design/icons"
+import { StyledLearnSuccess } from "@/modules/cards/components/learn-success/styles"
+import { Result } from "antd"
+import { StyledLearnCardButton } from "@/modules/cards/components/learn-page/styles"
+import { useNavigate } from "react-router-dom"
 
 export const LearnSuccess = () => {
+  const navigate = useNavigate()
+  const handleFinishLearn = () => {
+    navigate("/packs")
+  }
+
   return (
-    <>
-      <HeartOutlined />
-    </>
+    <StyledLearnSuccess>
+      <Result
+        status="success"
+        title="You have completed your learn."
+        extra={[
+          <StyledLearnCardButton onClick={handleFinishLearn}>
+            Go to Packs
+          </StyledLearnCardButton>,
+        ]}
+      />
+    </StyledLearnSuccess>
   )
 }
