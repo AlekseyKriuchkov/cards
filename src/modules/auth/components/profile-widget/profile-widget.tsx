89de85b6b7c5d605400c7f52989b5b8c3db5319f
Aleksey Kriuchkov <aleksejkriuchkov@gmail.com>
2024-08-09 17:11:53 +0200

    add skeleton in header widget

--- src/modules/auth/components/profile-widget/profile-widget.tsx
+++ src/modules/auth/components/profile-widget/profile-widget.tsx
@@ -8,29 +8,29 @@ import { WidgetButton } from "@/modules/auth/components/widget-button/widget-but
 import { ProfileLogOutButton } from "@/modules/auth/components/profile-logOut-button/profile-logOut-button"
 import { useNavigate } from "react-router-dom"
 import { UserOutlined } from "@ant-design/icons"
+import { Skeleton } from "antd"
 
 export const ProfileWidget = () => {
-  const { user, isAuthorized } = useAuth()
+  const { user, isAuthorized, isLoading } = useAuth()
   const navigate = useNavigate()
   const goToProfile = () => {
     navigate("/profile")
   }
+
+  if (isLoading) return <Skeleton.Input active />
+
   if (!isAuthorized) {
     return <WidgetButton />
   }
-  if (user?.avatar === " " || !user?.avatar) {
-    return (
-      <>
-        <StyledUserName onClick={goToProfile}>{user?.name}</StyledUserName>
-        <UserOutlined />
-        <ProfileLogOutButton />
-      </>
-    )
-  }
+
   return (
     <>
       <StyledUserName onClick={goToProfile}>{user?.name}</StyledUserName>
-      <StyledProfileWidgetAvatar src={user?.avatar} />
+      {user?.avatar === " " || !user?.avatar ? (
+        <UserOutlined />
+      ) : (
+        <StyledProfileWidgetAvatar src={user?.avatar} />
+      )}
       <ProfileLogOutButton />
     </>
   )
