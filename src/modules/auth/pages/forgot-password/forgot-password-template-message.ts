ae07d099897f3629a100f81227332a57c93139ae
Aleksey Kriuchkov <aleksejkriuchkov@gmail.com>
2024-08-12 15:57:28 +0200

    refactor forgot password

---
+++ src/modules/auth/pages/forgot-password/forgot-password-template-message.ts
@@ -0,0 +1,5 @@
+export const forgotPasswordTemplateMessage = `<div style="background-color: aqua; padding: 15px">
+password recovery link: 
+<a href='http://localhost:5173/set-new-password/$token$'>
+Create new password</a>
+</div>`
--- src/modules/auth/pages/forgot-password/index.tsx
+++ src/modules/auth/pages/forgot-password/index.tsx
@@ -1,45 +1,53 @@
 import React from "react"
 import { useAppDispatch, useAppSelector } from "@/app/hooks"
-import { LoginType } from "@/modules/auth/api/types"
 import { authThunk } from "@/modules/auth/auth.slice"
 import { StyledCard } from "@/modules/auth/pages/sign-in/styles"
-import { Button, Form, Input } from "antd"
+import { Button, Form, Input, Skeleton } from "antd"
 import { StyledNavLink } from "@/modules/auth/styles"
 import { StyledP, StyledText } from "@/modules/auth/pages/styles"
 import { CheckEmail } from "@/modules/auth/pages"
+import { forgotPasswordTemplateMessage } from "@/modules/auth/pages/forgot-password/forgot-password-template-message"
 
 export const ForgotPassword = () => {
   const dispatch = useAppDispatch()
   const isSuccess = useAppSelector((state) => state.auth.isSuccess)
-  const message = `<div style="background-color: aqua; padding: 15px">
-password recovery link: 
-<a href='http://localhost:5174/set-new-password/$token$'>
-link</a>
-</div>`
-  const onFinish = async (values: LoginType) => {
-    dispatch(authThunk.forgot({ ...values, message }))
+  const isLoading = useAppSelector((state) => state.auth.isLoading)
+
+  const sendMessageHandler = (values: { email: string }) => {
+    dispatch(
+      authThunk.forgot({
+        email: values.email,
+        message: forgotPasswordTemplateMessage,
+      }),
+    )
   }
+
   if (isSuccess) {
     return <CheckEmail />
   }
-  return (
-    <StyledCard title={"Forgot your password?"}>
-      <Form name="basic" onFinish={onFinish} autoComplete="off">
-        <Form.Item name="email">
-          <Input placeholder={"Email"} />
-        </Form.Item>
-        <StyledText>
-          Enter your email address and we will send you further instructions
-        </StyledText>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit" size={"large"} block={true}>
-            Send
-          </Button>
-        </Form.Item>
-        <StyledP>Did you remember your password?</StyledP>
-        <StyledNavLink to={"/sign-in"}>Sign In</StyledNavLink>
-      </Form>
+  return (
+    <StyledCard title="Forgot your password?">
+      {isLoading ? (
+        <Skeleton />
+      ) : (
+        <Form name="basic" onFinish={sendMessageHandler} autoComplete="off">
+          <Form.Item name="email">
+            <Input placeholder="Email" />
+          </Form.Item>
+          <StyledText>
+            Enter your email address and we will send you further instructions
+          </StyledText>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              Send
+            </Button>
+          </Form.Item>
+          <StyledP>Did you remember your password?</StyledP>
+          <StyledNavLink to="/sign-in">Sign In</StyledNavLink>
+        </Form>
+      )}
     </StyledCard>
   )
 }
